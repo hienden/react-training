9b484285b06dffab391dd2951a32aa5ccc64590f
hienden <hoanghien110397@gmail.com>
2023-08-01 17:01:40 +0700

    react event

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,27 @@
-import { useState } from "react";
 import "./App.css";
-import MyComponent from "./MyComponent";
-import ParentComponent from "./ParentComponent";
 
 function App() {
-  const [name, setName] = useState("tho");
+  const handleClick = (event) => {
+    console.log(event);
+  };
+  const handleChange = (event) => {
+    console.log("change input", event.target.value);
+  };
+  const abc = (event) => handleChange(event, "sdas", 23);
   return (
     <>
-      <button onClick={() => setName("hien")}>click</button>
-      <MyComponent name={name} />
-      <ParentComponent />
+      <button onClick={handleClick}>click</button>
+      <input type="text" onChange={abc} />
+      <select
+        name="cars"
+        id="cars"
+        onChange={(e) => console.log(e.target.value)}
+      >
+        <option value="volvo">Volvo</option>
+        <option value="saab">Saab</option>
+        <option value="mercedes">Mercedes</option>
+        <option value="audi343">Audi</option>
+      </select>
     </>
   );
 }
